d335dbeec8b7208f43ea3eef46307d85fd31b964
emirfer <42104197+emirfer@users.noreply.github.com>
2020-09-10 02:12:04 +0200

    Updates

---
+++ precache-manifest.0560eb8e889176d283564cccd0a05edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e997e1ba9f0fa57c9aa8d43930d746",
+    "url": "/vremenska_prognoza/index.html"
+  },
+  {
+    "revision": "016f32cfd6a587506f66",
+    "url": "/vremenska_prognoza/static/css/main.86aafdc7.chunk.css"
+  },
+  {
+    "revision": "0e0a8ef9663e9dff9606",
+    "url": "/vremenska_prognoza/static/js/2.e014e06c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/vremenska_prognoza/static/js/2.e014e06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016f32cfd6a587506f66",
+    "url": "/vremenska_prognoza/static/js/main.571a665b.chunk.js"
+  },
+  {
+    "revision": "4ada4034f79f8740b0ee",
+    "url": "/vremenska_prognoza/static/js/runtime-main.6679789c.js"
+  }
+]);
